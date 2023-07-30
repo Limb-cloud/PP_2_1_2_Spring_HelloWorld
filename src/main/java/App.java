3189a4da19dddc94970250c34a1f1e58ce2d4692
Клименко Вячеслав <lol.sava.pro@mail.ru>
2023-07-30 18:51:58 +0300

    Adding task Java pre-project. Практическая задача 2.1.3

---
+++ src/main/java/App.java
@@ -0,0 +1,20 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld bean =
+            (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld beanTwo =
+            (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.equals(beanTwo));
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat catTwo = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat.equals(catTwo));
+    }
+}
